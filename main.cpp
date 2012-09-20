c67610eece0170a4ce48a9e240a1b137046ec9c7
Volker Krause <volker.krause@kdab.com>
2012-09-20 16:14:55 +0200

    Add license header.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+/*
+    Copyright (C) 2012 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+    Author: Volker Krause <volker.krause@kdab.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 
@@ -168,4 +186,3 @@ int main(int argc, char** argv)
 
     delete[] configs;
 }
-
